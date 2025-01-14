b2be737802160ce7ad684e44c3232bc1117b874a
Bugsy <124417333+bugsbirb@users.noreply.github.com>
2025-01-14 20:25:14 +0000

    Make it faster

--- src/pages/panel/infraction/[id]/[infid].tsx
+++ src/pages/panel/infraction/[id]/[infid].tsx
@@ -11,7 +11,6 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Textarea } from '@/components/ui/textarea'
 
 export default function InfractionPage() {
   const router = useRouter()
